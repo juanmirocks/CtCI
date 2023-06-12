d9a520cfa2d3b076efe868f731e0f2f587681306
Dr. Juan Miguel Cejuela <i@juanmi.rocks>
2023-06-12 19:18:05 +0200

    add further test case & fix impl: a char can be repated any even number of times

--- Python3/ch01_arrays_and_strings/q04_palindrome_permutation.py
+++ Python3/ch01_arrays_and_strings/q04_palindrome_permutation.py
@@ -1,13 +1,38 @@
 
 
+from collections import Counter
+
+
 def is_permutation_of_palindrome_1(x: str) -> bool:
     """
     Notes:
-    * Observation: A palindrome must be a string where each character is repeated exactly 2 times (even sequence), or each character is repeated 2 times and one character is repeated only 1 time (odd sequence).
+    * Observation: A palindrome must be a string where each character is repeated an even number times (even sequence), or in addition, exceptionally, has a single character repeated 1 time (odd sequence).
     * Spaces are to be ignored (as per the question definition). That is: "tact coa" is equivalent to "tactcoa".
     * Case is ignored (as per the question assumptions).
+
+    Complexity:
+    * Time: O(n)
+    * Space: O(n)
     """
-    return False
+    # Add all string's characters to a hash-table-based counter -> time & space: O(n)
+    counter = Counter(x)
+
+    one_char_is_odd = False
+
+    # Iterate sorted by counts (DESC). In that way, we can easily
+    for elem, count in counter.items():
+        if elem == " ":
+            # ignore spaces
+            continue
+        else:
+            if (count % 2 == 1):
+                if one_char_is_odd:
+                    # two chars have count 1, so it's not a palindrome
+                    return False
+                else:
+                    one_char_is_odd = True
+
+    return True
 
 
 TEST_CASES = [
@@ -16,7 +41,8 @@ TEST_CASES = [
     ("aa", True),
     ("a a", True),
     ("a b", False),
-    ("tact coa", True)
+    ("tact coa", True),
+    ("tactcoapapa", True)
 ]
 
 def test():

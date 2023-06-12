acb79e8d9d7635769af3174f23b5794012873cfb
Dr. Juan Miguel Cejuela <i@juanmi.rocks>
2023-06-12 18:09:05 +0200

    Generalize the algorithm to a list (dictionary) of possible replacements

--- Python3/ch01_arrays_and_strings/q03_urlify.py
+++ Python3/ch01_arrays_and_strings/q03_urlify.py
@@ -1,15 +1,39 @@
 import urllib.parse
 from typing import Callable, MutableSequence
+from typing import NamedTuple
 
 # -----------------------------------------------------------------------------
 
+# Functions' signature
 signature = Callable[[str, int], str]
 
-REPL_IN = " "
-REPL_OUT = "%20"
-REPL_OUT_LEN = len(REPL_OUT)
-REPL_OUT_LEN_MINUS_1 = len(REPL_OUT) - 1
+#
+# Define the replacements as constants to generalize the algorithm.
+#
+class Replacement:
+    def __init__(self, old: str, new: str):
+        assert len(old) == 1, f"Old must be a single character: {old}"
 
+        self.old = old
+        self.new = new
+        self.new_len = len(self.new)
+        self.new_len_minus_1 = self.new_len - 1
+
+
+    def __repr__(self) -> str:
+        return f"Replacement({self.old}->{self.new})--{self.new_len_minus_1}"
+
+
+ORIGINAL_REPLACEMENT = Replacement(" ", "%20")
+
+REPLACEMENTS = [
+    ORIGINAL_REPLACEMENT,
+    # ... can add more
+]
+
+REPLACEMENTS_DICT: dict[str, Replacement] = {repl.old: repl for repl in REPLACEMENTS}
+
+# -----------------------------------------------------------------------------
 
 def urlify_1(x: str, l: int) -> str:
    return urlify_1_pure(x[0:l])
@@ -23,8 +47,12 @@ def urlify_2(x: str, l: int) -> str:
     return urlify_2_pure(x[0:l])
 
 def urlify_2_pure(x: str) -> str:
-    return x.replace(REPL_IN, REPL_OUT)
+    # Here we only do one replacement (the one asked for originally).
+    # Otherwise, we likely have to improve the algorithm, since Python always creates new (immutable) strings,
+    # which would result in a practical time & space complexity of O(x * len(REPLACEMENTS)). Either way, that would be linear too.
+    return x.replace(ORIGINAL_REPLACEMENT.old, ORIGINAL_REPLACEMENT.new)
 
+# -----------------------------------------------------------------------------
 
 def urlify_3(x: str, l: int) -> str:
     tmp = urlify_3_raw(list(x), l)
@@ -32,7 +60,7 @@ def urlify_3(x: str, l: int) -> str:
 
 def urlify_3_raw(x: MutableSequence[str], l: int) -> MutableSequence[str]:
     """
-    Replace spaces with "%20" (URL-escape) in a mutable sequence in place.
+    Replace spaces with "%20" (and al other `REPLACEMENTS`) in a mutable sequence in place.
 
     Note:
     * Idea: Start indexing & setting characters backwards. We can do this without having to re-edit the sequence because we already have a buffer at the end.
@@ -47,10 +75,12 @@ def urlify_3_raw(x: MutableSequence[str], l: int) -> MutableSequence[str]:
     after_idx = len(x) - 1
 
     while before_idx > -1:
-        if x[before_idx] == REPL_IN:
-            for i in range(REPL_OUT_LEN_MINUS_1, -1, -1):
-                x[after_idx] = REPL_OUT[i]
+        replacement = REPLACEMENTS_DICT.get(x[before_idx])
+        if replacement:
+            for i in range(replacement.new_len_minus_1, -1, -1):
+                x[after_idx] = replacement.new[i]
                 after_idx -= 1
+
         else:
             x[after_idx] = x[before_idx]
             after_idx -= 1
@@ -61,8 +91,8 @@ def urlify_3_raw(x: MutableSequence[str], l: int) -> MutableSequence[str]:
     return x
 
 # -----------------------------------------------------------------------------
-
-from typing import NamedTuple
+# Testing
+# -----------------------------------------------------------------------------
 
 class Input(NamedTuple):
     x: str

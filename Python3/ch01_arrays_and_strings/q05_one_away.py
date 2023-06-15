ced8299d6121e90020ee36d51f09662694e80f8e
Dr. Juan Miguel Cejuela <i@juanmi.rocks>
2023-06-15 17:19:00 +0200

    fix algorithm

--- Python3/ch01_arrays_and_strings/q05_one_away.py
+++ Python3/ch01_arrays_and_strings/q05_one_away.py
@@ -2,57 +2,43 @@ from collections import Counter
 from typing import Any, List, Sequence, Tuple
 
 
-def is_one_elem_away(a: Sequence[Any], b: Sequence[Any]) -> bool:
+def is_one_edit_away(a: Sequence[Any], b: Sequence[Any]) -> bool:
     """
     Complexity:
 
-    * Time: O(a + b)
-    * Space: O(a + b)
+    * Time: O(min(a, b))
+    * Space: O(1) - no extra data structures needed
     """
     if a == b:
-        # Return early for trivial case (zero edits, thus equal)
+        # Return early for trivial case of zero edits, thus equal
         return True
 
-    a_counter = Counter(a) # linear time & space on a's length
-    b_counter = Counter(b) # linear time & space on b's length
+    a_len = len(a)
+    b_len = len(b)
+    diff_len = a_len - b_len
 
-    is_one_count_away = False
+    if diff_len < -1 or diff_len > 1:
+        # Return early for trivial case of having a length different bigger than abs(1)
+        return False
 
-    if len(b_counter) < len(a_counter):
-        # Swap, so that we iterate only over the counter with the least number of keys, i.e., time O(min(a, b))
-        a_counter, b_counter = b_counter, a_counter
+    if diff_len == 1:
+        # swap to make sure a is the shortest sequence
+        a, b = b, a
 
-    for a_elem, a_elem_count in a_counter.items():
-        b_elem_count = b_counter.get(a_elem, None)
-        if b_elem_count is None:
-            if is_one_count_away or a_elem_count > 1:
-                return False
-            else:
-                is_one_count_away = True
+    for i in range(0, len(a)):
+        if a[i] == b[i]:
+            continue
         else:
-            diff = abs(a_elem_count - b_elem_count)
-            match diff:
-                case 0:
-                    pass
-                case 1:
-                    if is_one_count_away:
-                        return False
-                    else:
-                        is_one_count_away = True
-                case _:
-                    return False
+            # We found the first different elem
+            if diff_len == 0:
+                # if same length, the rest of both sequences must be equal
+                return a[i+1:] == b[i+1:]
+            else:
+                # else, the rest from the shortest sequence (including the current i elem ) must equal the rest of the longest sequence
+                return a[i:] == b[i+1:]
 
-            del b_counter[a_elem]
+    return True
 
-    # Now we need to verify that either b is empty or otherwise it contains a single element with a count of 1
-    match len(b_counter):
-        case 0:
-            return True
-        case 1:
-            _, b_last_count = b_counter.popitem()
-            return (b_last_count == 1)
-        case _:
-            return False
 
 
 # -----------------------------------------------------------------------------
@@ -74,7 +60,7 @@ TEST_CASES: List[Tuple[Tuple[str, str], bool]] = [
 
 
 def test():
-    fun = is_one_elem_away
+    fun = is_one_edit_away
 
     for input, expected_output in TEST_CASES:
         output = fun(*input)

134d51945d58ae9512ef260d859e085e8e9aefbf
Dr. Juan Miguel Cejuela <i@juanmi.rocks>
2023-06-20 12:32:31 +0200

    allow redefining equal function for test comparison

--- Python3/__util__.py
+++ Python3/__util__.py
@@ -4,8 +4,10 @@ from typing import Callable, TypeVar, Sequence
 Output = TypeVar("Output")
 
 
-def run_test_cases(test_cases: Sequence[tuple[tuple, Output]], *funs: Callable[..., Output]) -> None:
+def run_test_cases(test_cases: Sequence[tuple[tuple, Output]], *funs: Callable[..., Output], **kwargs) -> None:
+    equal = kwargs.get("equal", lambda x, y: x == y)
+
     for fun in funs:
         for input, expected_output in test_cases:
             output = fun(*input)
-            assert expected_output == output, f"in[{input}] -> expected_out[{expected_output}] vs. out[{output}]"
+            assert equal(expected_output, output), f"in[{input}] -> expected_out[{expected_output}] vs. out[{output}]"

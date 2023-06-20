9ec7c65e82da4b6b9918dc6b1bb4d7a64dc4fefd
Dr. Juan Miguel Cejuela <i@juanmi.rocks>
2023-06-20 13:56:07 +0200

    provide in-place answer for ch01/q07

--- Python3/ch01_arrays_and_strings/q07_rotate_matrix.py
+++ Python3/ch01_arrays_and_strings/q07_rotate_matrix.py
@@ -29,20 +29,47 @@ def rotate_matrix_1(x: npt.NDArray) -> npt.NDArray:
     return y
 
 
+def rotate_matrix_2(x: npt.NDArray) -> npt.NDArray:
+    """
+    Rotate matrix 90% (counterclockwise) IN-PLACE (mutable/destructive function).
+
+    Observations:
+    * Rotating a (quadratic) matrix can be seen as rotating "ring" by "ring", from the outer rings to the inner rings of the matrix.
+    * Elements can be changed in a loop of length 4 (the last element change closes the loop).
+    * We can derive all coordinates of 4 changed elements from the first element's coordinates.
+    * For matrixes' "center" (for matrixes with an odd n) are never changed.
+
+    Complexity:
+    * Time: O(n**2)
+    * Space: O(1)
+    """
+    assert (len(x.shape) == 2) and (x.shape[0] == x.shape[1]), f"Expect square matrix of size nxn -- Received array shape: {x.shape} (array: {x})"
+    n = x.shape[0]
+
+    for i in range(0, n-1):  # Note: we don't go to the last row of the ring, since its elements have been already changed
+        for j in range(i, n-1-i):  # Note: we don't go to the last column of the ring, since it's been changed already
+            # print(f"({i}, {j})")
+            # Loop of 4 changes
+            x[i, j], x[n-1-j, i], x[n-1-i, n-1-j], x[j, n-1-i] = x[j, n-1-i], x[i, j], x[n-1-j, i], x[n-1-i, n-1-j]
+
+    return x
+
+
 # -----------------------------------------------------------------------------
 
 from Python3.__util__ import run_test_cases
 
 
-_TEST_UP_TO_N = 10
+_TEST_FROM_TO = (1, 10)
 
 TEST_CASES = list(
-     map(lambda x: ((x, ), np.rot90(x)),
-                      (np.arange(1, n**2 + 1).reshape(n, n) for n in range(1, _TEST_UP_TO_N+1))))
+     map(lambda x: ((x, ), np.copy(np.rot90(x))),  # Copy/clone the expected output given by numpy's rot90, since it returns a view, which is the difficult to use when testing the mutable version
+                      (np.arange(1, n**2 + 1).reshape(n, n) for n in range(_TEST_FROM_TO[0], _TEST_FROM_TO[1]+1))))
 
 
 def test():
     run_test_cases(TEST_CASES,
                    rotate_matrix_1,
+                   rotate_matrix_2,
                    # Provide specific `equal`` function for array/matrix (numpy's equal implementation creates a new element-wise boolean matrix)
                    equal = lambda x, y: np.array_equal(x, y))

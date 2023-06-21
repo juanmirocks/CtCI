5214e31a727d405ee5b1e57a35c17c6fa9dc7a9f
Dr. Juan Miguel Cejuela <i@juanmi.rocks>
2023-06-21 17:25:05 +0200

    provide stub implementation & tests for ch01/q08

---
+++ Python3/ch01_arrays_and_strings/q08_zero_matrix.py
@@ -0,0 +1,58 @@
+import numpy as np
+import numpy.typing as npt
+
+
+# Q: Write an algorithm such tat if an element in an MxN matrix is 0, its entire row and column are set to 0.
+
+
+def zero_matrix_1(x: npt.NDArray) -> npt.NDArray:
+    return x
+
+
+# -----------------------------------------------------------------------------
+
+from Python3.__util__ import run_test_cases
+
+
+TEST_CASES: list[tuple[tuple[npt.NDArray], npt.NDArray]] = [
+    ((np.ndarray((0, 0)), ), np.ndarray((0, 0))), # edge case, empty matrix
+    ((np.arange(0, 0 + 1).reshape(1, 1), ), np.arange(0, 0 + 1).reshape(1, 1)), # basic case, matrix of size 1x1 with cell value 0
+    ((np.arange(1, 1 + 1).reshape(1, 1), ), np.arange(1, 1 + 1).reshape(1, 1)), # basic case, matrix of size 1x1 with cell value != 0 (esp. 1)
+    # Arbitrary case
+    ((
+      np.matrix([
+      [1, 1, 1, 1, 1],
+      [1, 0, 1, 1, 1],
+      [1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 0]
+      ]), ),
+      np.matrix([
+      [1, 0, 1, 1, 0],
+      [1, 0, 1, 1, 0],
+      [1, 0, 1, 1, 0],
+      [0, 0, 0, 0, 0]
+      ])),
+    # Arbitrary case
+    ((
+      np.matrix([
+      [0, 1, 1, 1, 1],
+      [1, 0, 1, 1, 1],
+      [1, 1, 0, 1, 1],
+      [1, 1, 1, 0, 1],
+      [1, 1, 1, 1, 1],
+      ]), ),
+      np.matrix([
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 1]
+      ])),
+]
+
+
+def test():
+    run_test_cases(TEST_CASES,
+                   zero_matrix_1,
+                   # Provide specific `equal`` function for array/matrix (numpy's equal implementation creates a new element-wise boolean matrix)
+                   equal = lambda x, y: np.array_equal(x, y))
